fb3ee4be2fd0e10c23940610665538ef62228c01
Ryan Scandrol <rkscandrol@gmail.com>
2023-08-27 11:45:02 -0400

    Additions

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello from Eclipse");
+		
+		System.out.println("Ryan is working locally on his branch");
 
 	}
 
